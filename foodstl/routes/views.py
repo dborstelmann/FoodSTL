c90c90bd9947d3cb2aad6665c1c4b15b33f24181
Daniel Borstelmann <dborstelmann@gmail.com>
2016-04-09 13:08:34 -0500

    landing page

--- foodstl/routes/views.py
+++ foodstl/routes/views.py
@@ -2,5 +2,9 @@ from foodstl import foodstl
 from flask import render_template, jsonify
 
 @foodstl.route('/')
+def landing():
+    return render_template('landing.html')
+
+@foodstl.route('/home')
 def home():
     return render_template('home.html')
